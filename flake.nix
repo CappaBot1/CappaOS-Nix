a763343856560623aa87f9349ee18bd2536a1c58
Casper Howse <casperhowse@gmail.com>
2026-07-07 13:27:31 +1200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "CappaOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    modules = [
+      "base"
+      "cappaos"
+      "cinnamon"
+      "copyq"
+      "creator"
+      "development"
+      "flameshot"
+      "gaming"
+      "i3"
+      "inside-virtual-machine"
+      "lightdm"
+      "ly"
+      "posy-cursor"
+      "virtualization"
+    ];
+  in {
+    nixosModules = builtins.listToAttrs
+      (
+        map
+          name: {
+            inherit name;
+            value = import ./${name}.nix;
+          }
+          modules
+      );
+  };
+}
